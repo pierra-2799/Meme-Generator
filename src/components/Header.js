2564054e3d5eaba9d35ca785bbc01f93d1db11be
pierra2799 <shah.mitesh2799@gmail.com>
2021-03-12 17:54:18 +0530

    Commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Header extends React.Component{
+    render(){
+        return(
+            <div className="Header">
+                <h1>Meme Generator</h1>
+            </div>
+        )
+    }
+}
+
+export default Header
